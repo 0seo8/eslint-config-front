647e6033d40d00fcb339b14fe1cb55da982dfe1c
youngseo <00seo23@gmail.com>
2024-03-02 20:15:30 +0900

    fix: devDependencies->dependencdies로 변경

--- src/rn.js
+++ src/rn.js
@@ -1,136 +1,137 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    env: {
-        browser: true,
-        es6: true,
-        jest: true,
-        node: true,
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'import',
+    'react-native',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.native.js'],
+      },
     },
-    plugins: [
-        '@typescript-eslint',
-        'react',
-        'react-hooks',
-        'import',
-        'react-native',
-        'prettier',
+  },
+  rules: {
+    'react-native/no-raw-text': 'off',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-color-literals': 'off',
+    'no-param-reassign': 'warn',
+    'react/jsx-no-useless-fragment': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-array-index-key': 'warn',
+    'react/require-default-props': 'off',
+    'react/function-component-definition': 'off',
+    'react/prop-types': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
+    'react-hooks/exhaustive-deps': [
+      'error',
+      {
+        enableDangerousAutofixThisMayCauseInfiniteLoops: true,
+      },
     ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:react-native/all',
-        'plugin:prettier/recommended',
+    'react-native/no-unused-styles': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-named-as-default': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
     ],
-    settings: {
-        'import/resolver': {
-            'babel-module': {},
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx', '.native.js'],
-            },
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'type',
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'unknown',
+        ],
+        pathGroups: [
+          {
+            pattern: '{react*,react*/**}',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@tanstack*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'recoil/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'hooks/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'navigations/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'screens/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'components/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'common/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react', '@tanstack'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
         },
-    },
-    rules: {
-        'react-native/no-inline-styles': 'warn',
-        'react-native/no-color-literals': 'off',
-        'no-param-reassign': 'warn',
-        'react/jsx-no-useless-fragment': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/no-array-index-key': 'warn',
-        'react/require-default-props': 'off',
-        'react/function-component-definition': 'off',
-        'react/prop-types': 'warn',
-        '@typescript-eslint/no-var-requires': 'off',
-        'react-hooks/exhaustive-deps': [
-            'error',
-            {
-                enableDangerousAutofixThisMayCauseInfiniteLoops: true,
-            },
-        ],
-        'react-native/no-unused-styles': 'error',
-        'import/no-extraneous-dependencies': 'off',
-        'import/no-named-as-default': 'off',
-        'import/prefer-default-export': 'off',
-        'import/extensions': [
-            'error',
-            'ignorePackages',
-            { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
-        ],
-        'import/order': [
-            'warn',
-            {
-                groups: [
-                    'type',
-                    'builtin',
-                    'external',
-                    'internal',
-                    'parent',
-                    'sibling',
-                    'index',
-                    'unknown',
-                ],
-                pathGroups: [
-                    {
-                        pattern: '{react*,react*/**}',
-                        group: 'external',
-                        position: 'before',
-                    },
-                    {
-                        pattern: '@**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                    {
-                        pattern: '@tanstack*',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                    {
-                        pattern: 'recoil/**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                    {
-                        pattern: 'hooks/**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                    {
-                        pattern: 'navigations/**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                    {
-                        pattern: 'screens/**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                    {
-                        pattern: 'components/**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                    {
-                        pattern: 'common/**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                ],
-                pathGroupsExcludedImportTypes: ['react', '@tanstack'],
-                'newlines-between': 'always',
-                alphabetize: {
-                    order: 'asc',
-                    caseInsensitive: true,
-                },
-            },
-        ],
-    },
+      },
+    ],
+  },
 };
